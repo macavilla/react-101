231a2fcd6c22fdbad1d5003515a2b7bdea1160bb
Maca <maca.villa.a@gmail.com>
2018-05-12 14:16:42 -0300

    primeros componentes, y React-Router-dom

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './routes/home';
+import About from './routes/about';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Fragment>
+          <Link to="/">Home</Link>
+          <br/>
+          <Link to="/about">About</Link>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </Fragment>
+      </Router>
     );
   }
 }
